f653972f87c15683f028fa76dd3f355735bafa67
jonyesteve <ergasianootropia@gmail.com>
2023-02-20 11:42:15 -0300

    new

---
+++ finalBrimgeist2/Assets/Scripts/Enemy/EnemyCreator.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.XR.Haptics;
+using static Types;
+
+public class EnemyCreator
+{
+    bool _usePool;
+    GameObject _enemyPrefab;
+    int _lastSpawnPosY1, _lastSpawnPosY2;
+    bool _exchange;
+
+    public EnemyController CreateNewEnemy()
+    {
+        Vector2 spawnVector = new Vector2(UnityEngine.Random.Range(10f, 11), UnityEngine.Random.Range(4, -4));
+        while (spawnVector.y == _lastSpawnPosY1 || spawnVector.y == _lastSpawnPosY2) spawnVector.y = UnityEngine.Random.Range(4, -4);
+        if (_exchange)
+        {
+            _exchange = false;
+            _lastSpawnPosY1 = (int) spawnVector.y;
+        }
+        else
+        {
+            _exchange = true;
+            _lastSpawnPosY2 = (int) spawnVector.y;
+        }
+        EnemyController enemyObj = _usePool ?
+            EnemyManager.current.enemyPool.Get().GetComponent<EnemyController>() 
+            : GameObject.Instantiate(_enemyPrefab).GetComponent<EnemyController>();
+        enemyObj.transform.SetPositionAndRotation(spawnVector, _enemyPrefab.transform.rotation);
+        return enemyObj;
+    }
+    public EnemyController CreateNewEnemy(Transform spawnVector)
+    {
+        EnemyController enemyObj = _usePool ?
+            EnemyManager.current.enemyPool.Get().GetComponent<EnemyController>()
+            : GameObject.Instantiate(_enemyPrefab).GetComponent<EnemyController>();
+        enemyObj.transform.SetPositionAndRotation(spawnVector.position, _enemyPrefab.transform.rotation);
+        return enemyObj;
+    }
+
+    public EnemyCreator(bool usePool, GameObject enemyPrefab)
+    {
+        _exchange = false;
+        _lastSpawnPosY1 = int.MinValue;
+        _lastSpawnPosY2 = int.MinValue;
+        _usePool = usePool;
+        _enemyPrefab = enemyPrefab;
+    }
+}
+
+[Serializable]
+public abstract class EnemyPack : IEnemy
+{
+
+    public int quantity { get => enemyQuant; set => enemyQuant = value; }
+    public EnemyType type { get => enemyType; set => enemyType = value; }
+    [SerializeField] int enemyQuant;
+    EnemyType enemyType;
+    public Transform[] positions;
+    public Transform positionReference;
+
+    public EnemyPack(int quantity, EnemyType type, Transform[] positions)
+    {
+        this.quantity = quantity;
+        this.type = type;
+        this.positions = positions;
+    }
+
+    public virtual void SpawnPack(int quantity)
+    {
+
+    }
+    protected Sprite GetSpriteFromType(EnemyType t) => EnemyManager.current._sprites[(int)t];
+    public void PackOrder(int quantity)
+    {
+        positions = new Transform[quantity];
+        switch (quantity)
+        {
+            case 1:
+                positions[0].position = positionReference.position + new Vector3(1, 0);
+                break;
+            case 2:
+                positions[0].position = positionReference.position + new Vector3(1, 0.7f);
+                positions[1].position = positionReference.position + new Vector3(1, -0.7f);
+                break;
+            case 3:
+                positions[2].position = positionReference.position + new Vector3(2, 0);
+                goto case 2;
+            case 4:
+                positions[2].position = positionReference.position + new Vector3(0.7f, 1.5f);
+                positions[3].position = positionReference.position + new Vector3(0.7f, -1.5f);
+                goto case 2;
+        }
+    }
+    protected EnemyController SpawnEnemy(EnemyType type, Transform position)
+    {
+        var enemyObj = EnemyManager.current.enemyCreator.CreateNewEnemy(position);
+        enemyObj.AssignValues(EnemyManager.current.defaultStats[(int)type], type, type == EnemyType.Boss1 || type == EnemyType.Boss2, GetSpriteFromType(type));
+        return enemyObj;
+    }
+}
+[Serializable]
+//public class HealerPack : EnemyPack
+//{
+//    public int frontlineEnemies;
+//    public EnemyType frontlineEnemiesType;
+//
+//    public HealerPack(int quantity, int frontlineEnemiesQuant, EnemyType frontlineEnemiesType, Transform[] positions, EnemyType type = //EnemyType.Healer) : base(quantity, type, positions)
+//    {
+//        this.frontlineEnemies = frontlineEnemiesQuant;
+//        this.frontlineEnemiesType = frontlineEnemiesType;
+//    }
+//}
+public class HealerPack : EnemyPack
+{
+    public EnemyController[] frontlineEnemies;
+    public EnemyType frontlineType;
+
+    public HealerPack(int quantity, int frontlineEnemies, EnemyType frontlineType, Transform[] positions, EnemyType type =
+        EnemyType.Healer) : base(quantity, type, positions)
+    {
+        this.type = type;
+        this.frontlineEnemies = new EnemyController[frontlineEnemies];
+        this.frontlineType = frontlineType;
+        PackOrder(frontlineEnemies);
+        for(int i=0; i < frontlineEnemies; i++)
+        {
+            this.frontlineEnemies[i] = SpawnEnemy(this.type, positions[i]);
+        }
+        SpawnEnemy(this.type, positionReference);
+    }
+
+}
+public interface IEnemy
+{
+    public int quantity { get; set; }
+    public EnemyType type { get; set; }
+}
+
---
+++ finalBrimgeist2/Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Pool;
+using static Types;
+using System;
+using System.Linq;
+using System.Threading;
+using Unity.VisualScripting;
+
+public class EnemyManager : MonoBehaviour
+{
+    
+    public BaseStats[] defaultStats;
+    public static EnemyManager current;
+    public ObjectPool<GameObject> enemyPool;
+    [SerializeField]public List<GameObject> loadedEnemies = new();
+    private bool _usePool;
+    public GameObject enemyPrefab;
+    public Sprite[] _sprites;
+    [SerializeField] bool _spawnEnemies;
+    public EnemyCreator enemyCreator;
+
+    //spawn
+    bool _spawnHealer, _spawnTank;
+    float _spawnTimer, waveTimerOffset;
+    int currentWave;
+    public static event Action WaveChange;
+
+    [Serializable]
+    public class Enemy : IEnemy
+    {
+        public int quantity { get => quant; set=>quant = value; }
+        public EnemyType type { get => enemyType; set=> enemyType = value;}
+
+        [SerializeField] int quant;
+        [SerializeField] EnemyType enemyType;
+        [SerializeField] HealerPack healerpack;
+        
+    }
+    [Serializable]
+    public class EnemyWave
+    {
+        public int wave;
+        public List<Enemy> enemies;
+    }
+    [SerializeField] List<EnemyWave> enemyWaves;
+    //enemies.Find(enemy => enemy.wave == 0).enemies.Find(i => i.enemyType == EnemyType.Striker).count--;
+
+    
+    private void Awake()
+    {
+        _usePool = true;
+        if (current == null) current = this;
+        else Destroy(gameObject);
+
+        enemyPool = new ObjectPool<GameObject>(() =>
+        {
+            return Instantiate(enemyPrefab);
+        }, enemy =>
+        {
+            enemy.SetActive(true);
+
+        }, enemy =>
+        {
+            enemy.SetActive(false);
+        }, enemy =>
+        {
+            Destroy(enemy);
+        }, false, 50, 100);
+
+        enemyCreator = new EnemyCreator(_usePool, enemyPrefab);
+        currentWave = 0;
+        for(int i=0; i < 15; i++)
+        {
+            loadedEnemies.Add(enemyPool.Get());
+        }
+        for (int i = 14; i >= 0; i--)
+        {
+            enemyPool.Release(loadedEnemies[i]);
+            loadedEnemies.Remove(loadedEnemies[i]);
+        }
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        _spawnTimer += Time.fixedDeltaTime;
+        if (_spawnTimer > 2 + waveTimerOffset)
+        {
+            _spawnTimer = 0;
+            WaveChanged();
+        }
+    }
+
+    //EnemyController SpawnEnemy(EnemyType type)
+    //{
+    //    var enemyObj = enemyCreator.CreateNewEnemy();
+    //    enemyObj.AssignValues(defaultStats[(int)type], type, type == EnemyType.Boss1 || type == EnemyType.Boss2, GetSpriteFromType(type));
+    //    return enemyObj;
+    //}
+
+    //HealerPack SpawnPack(int quantity, int frontlineQuant,EnemyType frontlineType, Transform[] positions)
+    //{
+    //    List<EnemyController> frontlineEnemies= new List<EnemyController>();
+    //    var enemyPack = new HealerPack(quantity, frontlineQuant, frontlineType, positions);
+    //    for(int i =0; i<frontlineQuant; i++)
+    //    {
+    //        enemyPack.quantity--;
+    //        frontlineEnemies.Add(SpawnEnemy(frontlineType));
+    //    }
+    //    SpawnEnemy(EnemyType.Healer);
+    //
+    //    return enemyPack;
+    //}
+
+
+    //public IEnumerator SpawningRoutine()
+    //{
+    //    float spawnTimer = 0;
+    //    while(_spawnEnemies == true)
+    //    {
+    //        spawnTimer += Time.fixedDeltaTime;
+    //
+    //        var currentEnemies = enemyWaves.Find(enemyWave => enemyWave.wave == currentWave).enemies;
+    //        var commonEnemy = currentEnemies.Find(enemy => enemy.type == EnemyType.Striker);
+    //        if (commonEnemy.quantity != 0 && spawnTimer >1.5f)
+    //        {
+    //            spawnTimer = 0;
+    //            commonEnemy.quantity--;
+    //            SpawnEnemy(commonEnemy.type);
+    //        }
+    //        if(currentEnemies.Contains(currentEnemies.Find(enemy => enemy.type == EnemyType.Healer)))
+    //        {
+    //            var enemy = currentEnemies.Find(enemy => enemy.type == EnemyType.Healer);
+    //            
+    //        }
+    //
+    //        yield return new WaitForFixedUpdate();
+    //    }
+    //}
+
+    Sprite GetSpriteFromType(EnemyType t) => _sprites[(int)t];
+
+    void WaveChanged()
+    {
+        currentWave++;
+        WaveChange();
+    }
+}
